e622e14e7b521c3aee22051d6c48a548c87ba7a3
Gilbert Macabuag <gmacabuag@iMac.local>
2019-06-14 22:03:39 -0400

    Added displaying list of search results

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const ImageList = () => {
-  return <div>ImageList</div>;
+const ImageList = props => {
+  console.log(props.images);
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={description} />;
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
